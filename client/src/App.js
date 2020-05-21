13e1c4db1cd2434a8adb67d6329201d527e48cf5
mfernandezfreire <mfernandezfreire@outlook.com>
2020-05-21 12:44:27 +0200

    login Signup class method works

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,76 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import { Switch, Route } from "react-router-dom";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Navbar from "./components/Navbar/Navbar";
+import Signup from "./components/Auth/Signup";
+import Login from "./components/Auth/Login";
+
+import AuthService from "./components/Auth/Auth-service";
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { loggedInUser: null };
+    this.service = new AuthService();
+  }
+
+  fetchUser() {
+    if (this.state.loggedInUser === null) {
+      this.service
+        .loggedin()
+        .then((response) => {
+          this.setState({
+            loggedInUser: response,
+          });
+        })
+        .catch((err) => {
+          this.setState({
+            loggedInUser: false,
+          });
+        });
+    }
+  }
+
+  getTheUser = (userObj) => {
+    this.setState({
+      loggedInUser: userObj,
+    });
+  };
+
+  render() {
+    if (this.state.loggedInUser) {
+      return (
+        <div className="App">
+          <Navbar
+            userInSession={this.state.loggedInUser}
+            getUser={this.getTheUser}
+          />
+        </div>
+      );
+    } else {
+      return (
+        <div className="App">
+          <Navbar
+            userInSession={this.state.loggedInUser}
+            getUser={this.getTheUser}
+          />
+          <Switch>
+            <Route
+              exact
+              path="/"
+              render={() => <Login getUser={this.getTheUser} />}
+            />
+            <Route
+              exact
+              path="/signup"
+              render={() => <Signup getUser={this.getTheUser} />}
+            />
+          </Switch>
+        </div>
+      );
+    }
+  }
 }
 
 export default App;
